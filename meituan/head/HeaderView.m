d68e962987d04fa15f74f2de7e163996e3f85be2
YONGMA <yong.ma@thoughtworks.com>
2020-03-28 20:42:12 +0800

    add animation

--- meituan/head/HeaderView.m
+++ meituan/head/HeaderView.m
@@ -10,7 +10,7 @@
 
 @interface  HeaderView()
 @property (weak, nonatomic) IBOutlet UIButton *searchButton;
-
+@property (strong, nonatomic) NSArray<NSString *> *array;
 @end
 
 
@@ -20,12 +20,19 @@
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect {
-  [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(task) userInfo:nil repeats:YES];
+    _array=@[@"XXX酒店",@"肉夹馍来了",@"纯净水",@"你说啥",@"不知道"];
+  [NSTimer scheduledTimerWithTimeInterval:3 target:self selector:@selector(task) userInfo:nil repeats:YES];
 }
 
 -(void) task{
     static int count=0;
-    [self.searchButton setTitle:[NSString stringWithFormat:@"时事新闻%d",count] forState:UIControlStateNormal];
+    CGRect rect = self.searchButton.titleLabel.frame;
+    [UIView animateWithDuration:0.3 animations:^{
+        self.searchButton.titleLabel.frame  = CGRectMake(rect.origin.x, rect.origin.y - 50, rect.size.width, rect.size.height);
+    }];
+    self.searchButton.titleLabel.frame  = rect;
+     [self.searchButton setTitle:[self.array objectAtIndex:count % self.array.count] forState:UIControlStateNormal];
     count++;
 }
+
 @end
